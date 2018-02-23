158c70fcdb9ff7ca14422f80ec60db2b5b19ef8e
Jackson Markowski <markowski.jackson@gmail.com>
2018-02-22 19:56:20 -0700

    Added user permission for dangerous permissions

--- app/src/main/java/cuteam17/cuteam17phone/MainActivity.java
+++ app/src/main/java/cuteam17/cuteam17phone/MainActivity.java
@@ -1,11 +1,17 @@
 package cuteam17.cuteam17phone;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-	MessagingServiceReceiver s = new MessagingServiceReceiver();
+	private static final int REQUEST_CODE_PERMISSIONS_LIST = 117;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +19,40 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
+			establishPermissions();
 			//ToDo: start bluetooth
-			//MessageService.startActionFoo(getApplicationContext(), "no", "noo");
 		}
+		//MessageService.startActionFoo(getApplicationContext(), "no", "noo");
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case REQUEST_CODE_PERMISSIONS_LIST: {
+				//ToDo: check each permission result
+
+
+			}
+		}
+	}
+
+	private void establishPermissions() {
+		List<String> permissionsList = new ArrayList<String>();
+
+		addPermissionToList(permissionsList, Manifest.permission.RECEIVE_SMS);
+		addPermissionToList(permissionsList, Manifest.permission.READ_PHONE_STATE);
+		addPermissionToList(permissionsList, Manifest.permission.READ_CONTACTS);
+
+		if (!permissionsList.isEmpty()) {
+			requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_PERMISSIONS_LIST);
+		}
+
+	}
+
+	private boolean addPermissionToList(List<String> permissionsList, String permission) {
+		if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+			permissionsList.add(permission);
+			return true;
+		} else return false;
 	}
 }
