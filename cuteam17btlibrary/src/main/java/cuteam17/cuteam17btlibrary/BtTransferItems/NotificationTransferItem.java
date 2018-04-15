64341fafebcdc401d13ea267816a7a5d8b0e2219
Jackson Markowski <markowski.jackson@gmail.com>
2018-04-15 17:00:54 -0600

    Populate NotificationTransferItem from notification extras

--- cuteam17btlibrary/src/main/java/cuteam17/cuteam17btlibrary/BtTransferItems/NotificationTransferItem.java
+++ cuteam17btlibrary/src/main/java/cuteam17/cuteam17btlibrary/BtTransferItems/NotificationTransferItem.java
@@ -2,29 +2,114 @@ package cuteam17.cuteam17btlibrary.BtTransferItems;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.graphics.drawable.Icon;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.io.Serializable;
 
-public class NotificationTransferItem extends BtTransferItem implements Serializable {
+public class NotificationTransferItem extends BtTransferItem {
 
-	private String packageName;
+	private String applicationLabel;
 	private String title;
+	private String titleBig;
 	private String text;
+	private String bigText;
+	private String summaryText;
+	private boolean showWhen = false;
+	private long when;
+	//private Icon largeIcon;
+	//private Icon smallIcon;
+
+	//private Notification.Style style;
 
 	public NotificationTransferItem() {
 		super(TransferItemType.NOTIFICATION);
 	}
 
-	public NotificationTransferItem(String packageName, String title, String text) {
-		super(TransferItemType.NOTIFICATION);
-		this.packageName = packageName;
+	public void setApplicationLabel(String applicationLabel) {
+		this.applicationLabel = applicationLabel;
+	}
+
+	public String getApplicationLabel() {
+		return applicationLabel;
+	}
+
+	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitleBig(String titleBig) {
+		this.titleBig = titleBig;
+	}
+
+	public String getTitleBig() {
+		return titleBig;
+	}
+
+	public void setText(String text) {
 		this.text = text;
 	}
 
-	public String getPackageName() {
-		return packageName;
+	public String getText() {
+		return text;
 	}
+
+	public void setBigText(String bigText) {
+		this.bigText = bigText;
+	}
+
+	public String getBigText() {
+		return bigText;
+	}
+
+	public void setSummaryText(String summaryText) {
+		this.summaryText = summaryText;
+	}
+
+	public String getSummaryText() {
+		return summaryText;
+	}
+
+	public void setShowWhen(Boolean showWhen) {
+		this.showWhen = showWhen;
+	}
+
+	public boolean getShowWhen() {
+		return showWhen;
+	}
+
+	public void setWhen(long when) {
+		this.when = when;
+	}
+
+	public long getWhen() {
+		return when;
+	}
+
+	/*
+	public void setLargeIcon(Icon largeIcon) {
+		this.largeIcon = largeIcon;
+	}
+
+	public Icon getLargeIcon() {
+		return largeIcon;
+	}
+
+	public void setSmallIcon(Icon smallIcon) {
+		this.smallIcon = smallIcon;
+	}
+
+	public Icon getSmallIcon() {
+		return smallIcon;
+	}
+	*/
+
+
 
 
 
--- cuteam17phone/src/main/java/cuteam17/cuteam17phone/NotificationListener.java
+++ cuteam17phone/src/main/java/cuteam17/cuteam17phone/NotificationListener.java
@@ -4,12 +4,15 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.ImageView;
 
 import cuteam17.cuteam17btlibrary.BtTransferItems.NotificationTransferItem;
 import cuteam17.cuteam17btlibrary.BtTransferItems.SMSTransferItem;
@@ -27,28 +30,41 @@ public class NotificationListener extends NotificationListenerService {
 		PreferenceManager prefs = new PreferenceManager(this);
 
 		if (prefs.getNotificationPref()) {
-			String pack = sbn.getPackageName();
 			Notification notification = sbn.getNotification();
 			Bundle extras = notification.extras;
 
+			NotificationTransferItem item = new NotificationTransferItem();
 
-			String title = extras.getString("android.title");
-			String titleBig = extras.getString("android.title.big");
-			String text = extras.getString("android.text");
-			Icon id = notification.getLargeIcon();
-			Log.d("newNot", pack);
-			//NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(this);
-			//mNotificationManager.notify(1, newNot);
+			item.setApplicationLabel(getApplicationLabel(sbn.getPackageName()));
+			item.setWhen(notification.when);
+			item.setShowWhen(extras.getBoolean("android.showWhen"));
+			item.setTitle(extras.getString("android.title"));
+			item.setTitleBig(extras.getString("android.title.big"));
+			item.setText(extras.getString("android.text"));
+			item.setBigText(extras.getString("android.bigText"));
+			item.setSummaryText(extras.getString("android.summaryText"));
 
-			/*
-			NotificationTransferItem notificationToTransfer = new NotificationTransferItem(pack, title, text);
+			//item.setLargeIcon(notification.getLargeIcon());
+			//item.setSmallIcon(notification.getSmallIcon());
+
+
+
+			//android.template=android.app.Notification$InboxStyle
+			//android.subText
+
+			//android.reduced.images
+			//android.picture
+
+
+
 
 			Intent btIntent = new Intent(this, PhoneBtTransferService.class);
 			btIntent.setAction(PhoneBtTransferService.BT_WRITE);
 			Bundle bundle = new Bundle();
-			bundle.putSerializable(PhoneBtTransferService.INTENT_EXTRA_WRITE, notificationToTransfer);
+			bundle.putSerializable(PhoneBtTransferService.INTENT_EXTRA_WRITE, item);
 			btIntent.putExtras(bundle);
-			startService(btIntent);*/
+			startService(btIntent);
+
 		}
 
 	}
@@ -56,5 +72,16 @@ public class NotificationListener extends NotificationListenerService {
 	@Override
 	public void onNotificationRemoved(StatusBarNotification sbn) {
 
+	}
+
+	private String getApplicationLabel(String packageName) {
+		PackageManager pm = getApplicationContext().getPackageManager();
+		ApplicationInfo info;
+		try {
+			info = pm.getApplicationInfo(packageName, 0);
+		} catch (PackageManager.NameNotFoundException e) {
+			info = null;
+		}
+		return (String) (info != null ? pm.getApplicationLabel(info) : "");
 	}
 }
